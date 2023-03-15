c7e012526f3a549fe47838d57fb767bdd4c66496
Eduardo Aguiar da Silva <91293635+EduBardoADS@users.noreply.github.com>
2023-03-15 18:49:44 -0300

    Create exemplo04.py

---
+++ exemplo04.py
@@ -0,0 +1,6 @@
+#exemplo 04 - media
+nota1 = float(input('digite a primeira nota: '))
+nota2 = float(input('digite a segunda nota: '))
+media = (nota1+nota2)/2
+print("A sua média é:  %.2f " %media)
+#print("A sua média é: %.2f entr %.2f e %.2f" %(media, nota1, nota2))
